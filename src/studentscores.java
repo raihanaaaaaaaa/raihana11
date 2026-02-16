ab19870c7bed76272b1e7fb0c12d3164da758554
Admin <raihana.abdimutalipova@alatoo.edu.kg>
2026-02-16 11:44:04 +0600

    .

--- src/studentscores.java
+++ src/studentscores.java
@@ -5,7 +5,7 @@ public class studentscores {
     public static void main(String[] args) {
 
 
-        // Create & Put
+        // Create & Put е
         HashMap<String, Integer> studentScores = new HashMap<>();
         studentScores.put("Aida", 85);
         studentScores.put("Azat", 92);
